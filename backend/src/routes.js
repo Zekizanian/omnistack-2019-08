acaeb68ebf6d03a129fbc5e9edc67b598d022ece
André Ventura <andre.ventura@cupula.local>
2019-08-07 14:52:53 -0300

    criação do front end através od react app

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,15 +2,20 @@ const express = require('express');
 
 const routes = express.Router();
 
+const DevController = require('./controllers/DevController')
+const LikeController = require('./controllers/LikeController')
+const DislikeController = require('./controllers/DislikeController')
+
+
 //GET, POST, PUT, DELETE
 routes.get('/', (request,response) => {
     return response.json({message:`Hello ${request.query.name}`})
 });
 
-routes.post('/devs' , (request, response)=> {
-    console.log(request.body)
-    return response.json({ok:true})
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes',LikeController.store);
+routes.post('/devs/:devId/dislikes',DislikeController.store);
 
 
 module.exports = routes;
